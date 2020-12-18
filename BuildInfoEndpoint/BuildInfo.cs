0e737033f9c7a68d8ece46a4109f28eebfed79bf
Bo Søborg Petersen <bo@petersen.dk>
2020-12-18 12:25:30 +0100

    Initial commit

---
+++ BuildInfoEndpoint/BuildInfo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace BuildInfoEndpoint
+{
+    public class BuildInfo
+    {
+        private static readonly string _buildFileName = ".buildinfo.json";
+        public string GitHash { get; } = "000000";
+        public string ShortGitHash => GitHash.Count() >= 6 ? GitHash.Substring(0, 6) : "000000";
+        public string Version { get; } = "000000";
+        public string DevopsRepoUrl { get; } = "000000";
+        public string DevopsPipelineUrl { get; } = "00000000.0";
+
+        public BuildInfo(IHostEnvironment hostEnvironment)
+        {
+            var buildFilePath = Path.Combine(hostEnvironment.ContentRootPath, _buildFileName);
+
+            if (File.Exists(buildFilePath))
+            {
+                dynamic jToken = JToken.Parse(File.ReadAllText(buildFilePath));
+
+                GitHash = jToken.GitHash ?? GitHash;
+                Version = jToken.Version ?? Version;
+                DevopsRepoUrl = jToken.DevopsRepoUrl ?? DevopsRepoUrl;
+                DevopsPipelineUrl = jToken.DevopsPipelineUrl ?? DevopsPipelineUrl;
+            }
+
+            Console.WriteLine($"Powered by {RuntimeInformation.FrameworkDescription} and deployed from commit {ShortGitHash} with version {Version}");
+        }
+    }
+}
---
+++ BuildInfoEndpoint/BuildInfoExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Hosting;
+using System.Runtime.InteropServices;
+
+namespace BuildInfoEndpoint
+{
+    public static class BuildInfoExtensions
+    {
+        public static IEndpointConventionBuilder MapBuildInfoEndpoint(this IEndpointRouteBuilder endpoints, string path, IHostEnvironment env)
+        {
+            return endpoints.MapGet(path, async context =>
+            {
+                var _info = new BuildInfo(env);
+                context.Response.ContentType = "text/HTML";
+                var repoLink = $"<a href=\"{_info.DevopsRepoUrl}\">{_info.ShortGitHash}</a>";
+                var pipelineLink = $"<a href=\"{_info.DevopsPipelineUrl}\">{_info.Version}</a>";
+                await context.Response.WriteAsync($"Powered by {RuntimeInformation.FrameworkDescription} and deployed from commit {repoLink} via build {pipelineLink}");
+            });
+        }
+    }
+}
